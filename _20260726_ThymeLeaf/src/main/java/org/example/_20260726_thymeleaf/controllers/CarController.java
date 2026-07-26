817ced2a18ca16d727201bdea48d573eff748125
bmailggreen <bmailggreen@gmail.com>
2026-07-26 14:56:19 +0200

    Added server side form plus post sending to add data.

--- _20260726_ThymeLeaf/src/main/java/org/example/_20260726_thymeleaf/controllers/CarController.java
+++ _20260726_ThymeLeaf/src/main/java/org/example/_20260726_thymeleaf/controllers/CarController.java
@@ -1,15 +1,31 @@
 package org.example._20260726_thymeleaf.controllers;
 
+import org.example._20260726_thymeleaf.models.Car;
 import org.example._20260726_thymeleaf.services.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class CarController {
     @Autowired
     CarService carService;
+
+    @PostMapping("/addCar")
+//    @ResponseBody
+    private String addCar(/*Model model, */
+            /*@ModelAttribute*/ Car car) {
+        //System.out.println(car);
+        carService.cars.add(car);
+//        model.addAttribute("cars", carService.cars);
+//        return "cars";
+//        return "Adom a kocsikat";
+        return "redirect:/";
+    }
 
     @GetMapping("/")
     public String mainPage(Model model) {
@@ -18,6 +34,8 @@ public class CarController {
         //System.out.println(carService.cars);
         model.addAttribute(
                 "cars", carService.cars);
+        model.addAttribute(
+                "car", new Car("Toyota", 230, "Silver"));
         return "cars";
     }
 
--- _20260726_ThymeLeaf/src/main/java/org/example/_20260726_thymeleaf/services/CarService.java
+++ _20260726_ThymeLeaf/src/main/java/org/example/_20260726_thymeleaf/services/CarService.java
@@ -22,6 +22,6 @@ public class CarService {
     }
 
     public CarService() {
-        fillCarTestData();
+//        fillCarTestData();
     }
 }
